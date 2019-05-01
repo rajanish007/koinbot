d9db2546d4d83b30dce3b461706d94a8eb987016
Rajanish Ranjan <ranjan.rajanish0@gmail.com>
2019-05-02 01:41:51 +0530

    initial commit
    Signed-off-by: Rajanish Ranjan <ranjan.rajanish0@gmail.com>

---
+++ src/test/java/com/epex/koinbot/KoinbotApplicationTests.java
@@ -0,0 +1,16 @@
+package com.epex.koinbot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KoinbotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
