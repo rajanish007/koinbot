2ab1369743f10738f2e814ca1843fdb3716e7f86
Rajanish Ranjan <ranjan.rajanish0@gmail.com>
2019-05-06 01:36:37 +0530

    Duplicate identifier issue fixed
    Signed-off-by: Rajanish Ranjan <ranjan.rajanish0@gmail.com>

--- src/main/java/com/epex/koinbot/entities/CurrencyPortfolio.java
+++ src/main/java/com/epex/koinbot/entities/CurrencyPortfolio.java
@@ -2,6 +2,7 @@ package com.epex.koinbot.entities;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 
 import java.math.BigDecimal;
@@ -11,7 +12,7 @@ import java.math.BigDecimal;
 public class CurrencyPortfolio {
 
     @Id
-    private long id;
+    private ObjectId id;
 
     private BigDecimal highest_bid;
 
